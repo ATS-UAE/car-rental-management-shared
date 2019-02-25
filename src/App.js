a4ed85ff3d39badf99cd213cee344b4ad5fdd362
ramilamparo <ramilamparo@gmail.com>
2019-02-25 22:38:54 +0400

    Add CSSBaseline

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import CssBaseline from '@material-ui/core/CssBaseline';
 
+import AppBar from "./components/presentational/AppBar";
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+        <AppBar />
+        <CssBaseline />
+			</div>
+		);
+	}
 }
 
 export default App;
