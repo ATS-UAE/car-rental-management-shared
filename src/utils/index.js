efae9f8ed2cefcae484516fa42acc8e5512ec479
ramilamparo <ramilamparo@gmail.com>
2019-02-27 03:21:52 +0400

    Add TextField with validation component.

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,3 +8,37 @@ export function runIfExistFunction(func, parent, args) {
 		}
 	};
 }
+
+export function Validator(test, errorMessage) {
+	this.test = test;
+	this.error = errorMessage;
+}
+
+// Returns errored validators.
+Validator.runThroughValidators = function runThroughValidators(
+	validators = [],
+	validatee
+) {
+	let errors = [];
+	for (let i = 0; i < validators.length; i++) {
+		let validator = validators[i].test.bind(validators[i].test);
+		if (validatee !== undefined && !validator(validatee))
+			errors.push(validators[i]);
+	}
+	return errors;
+};
+
+export const validators = {
+	username: [
+		new Validator(
+			v => /.{5,}/.test(v),
+			"Username should be greater than 5 characters."
+		)
+	],
+	password: [
+		new Validator(
+			v => /.{8,}/.test(v),
+			"Password should be greater than 8 characters."
+		)
+	]
+};
