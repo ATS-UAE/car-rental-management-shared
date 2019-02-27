4d87ee12b840dfebacf075abfc0bd0e18c6201d2
ramilamparo <ramilamparo@gmail.com>
2019-02-27 04:46:57 +0400

    Add router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { BrowserRouter } from "react-router-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 class App extends Component {
+	state = {
+		textValue: ""
+	};
 	render() {
 		return (
 			<div className="App">
 				<BrowserRouter>
 					<Fragment />
 				</BrowserRouter>
+
 				<CssBaseline />
 			</div>
 		);
