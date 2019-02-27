64c7f7b0e2d62f36d72327ce91bb1086325c557c
ramilamparo <ramilamparo@gmail.com>
2019-02-27 04:10:12 +0400

    Setup react-router.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
-import CssBaseline from '@material-ui/core/CssBaseline';
+import { BrowserRouter } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 import AppBar from "./components/presentational/AppBar";
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-        <AppBar />
-        <CssBaseline />
+				<BrowserRouter>
+					<AppBar />
+				</BrowserRouter>
+				<CssBaseline />
 			</div>
 		);
 	}
