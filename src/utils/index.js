e69750e35a4457e8acbd29f3c19f92ceb2569bab
ramilamparo <ramilamparo@gmail.com>
2019-03-07 06:26:30 +0400

    Add response builder class.

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,4 +4,30 @@ const asyncForEach = async (array, cb) => {
 	}
 };
 
-module.exports = { asyncForEach };
+function ResponseBuilder() {
+	this.code = 500;
+	this.errors = [];
+	this.message = "Unknown server error";
+	this.success = false;
+	this.data = null;
+}
+
+ResponseBuilder.prototype.setData = function(data) {
+	this.data = data;
+};
+ResponseBuilder.prototype.setSuccess = function(success) {
+	this.success = success;
+};
+ResponseBuilder.prototype.appendError = function(error) {
+	this.errors.push(error);
+};
+ResponseBuilder.prototype.setCode = function(code) {
+	this.code = code;
+};
+ResponseBuilder.prototype.setMessage = function(message) {
+	this.message = message;
+};
+ResponseBuilder.prototype.getResponse = function() {
+	return ({ error, message, success, data } = this);
+};
+module.exports = { asyncForEach, ResponseBuilder };
