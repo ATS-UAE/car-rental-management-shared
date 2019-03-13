8f28f172894718679afc0732fd9d6187cc3682f2
ramilamparo <ramilamparo@gmail.com>
2019-03-13 05:24:56 +0400

    Update user API.

--- src/routes/users.js
+++ src/routes/users.js
@@ -9,67 +9,52 @@ const { ROLES, RESOURCES, accessControl, op } = require("../rbac/init");
 const { CREATE, READ, UPDATE, DELETE } = op;
 const db = require("../models");
 const { errorCodes } = require("../utils/variables");
-const { ResponseBuilder, sendInviteToken } = require("../utils");
+const { ResponseBuilder, pickFields } = require("../utils");
 
-router.use(requireLogin);
-router.use(disallowGuests);
-
-router.get("/", async ({ user }, res) => {
+router.get("/", requireLogin, disallowGuests, async ({ user }, res) => {
 	let response = new ResponseBuilder();
-	try {
-		await accessControl.can(user.role.name, `${RESOURCES.USERS}:${READ}`);
+	let accessible = await accessControl.can(
+		user.role.name,
+		`${RESOURCES.USERS}:${READ}`
+	);
+	if (accessible) {
 		let users = await db.User.findAll({
 			include: [{ model: db.Role, as: "role" }]
 		});
-
-		response.setData(
-			users.map(
-				({
-					id,
-					username,
-					firstName,
-					lastName,
-					gender,
-					email,
-					mobileNumber,
-					lastLogin,
-					userImageSrc,
-					licenseImageSrc,
-					approved,
-					blocked,
-					createdAt,
-					updatedAt,
-					userCreatorId,
-					role
-				}) => ({
-					id,
-					username,
-					firstName,
-					lastName,
-					gender,
-					email,
-					mobileNumber,
-					lastLogin,
-					userImageSrc,
-					licenseImageSrc,
-					approved,
-					blocked,
-					createdAt,
-					updatedAt,
-					userCreatorId,
-					role: { id: role.id, name: role.name }
-				})
-			)
-		);
+		let result = users.map(user => ({
+			...pickFields(
+				[
+					"id",
+					"username",
+					"firstName",
+					"lastName",
+					"gender",
+					"email",
+					"mobileNumber",
+					"lastLogin",
+					"userImageSrc",
+					"licenseImageSrc",
+					"approved",
+					"blocked",
+					"createdAt",
+					"updatedAt",
+					"userCreatorId"
+				],
+				user.get({ plain: true })
+			),
+			role: pickFields(["id", "name"], user.get({ plain: true }).role)
+		}));
 		response.setSuccess(true);
 		response.setCode(200);
 		response.setMessage(`Found ${users.length} users.`);
-	} catch (err) {
+		response.setData(result);
+	} else {
 		response.setCode(errorCodes.UNAUTHORIZED.statusCode);
 		response.setMessage(errorCodes.UNAUTHORIZED.message);
 		response.setSuccess(false);
 		res.status(errorCodes.UNAUTHORIZED.statusCode);
 	}
+
 	res.json(response);
 });
 
@@ -78,68 +63,67 @@ router.post("/", async ({ user, body }, res) => {
 	// an email will be sent to the invitee.
 	// Else, create directly with approved = true.
 	let response = new ResponseBuilder();
-	try {
-		await accessControl.can(user.role.name, `${RESOURCES.USERS}:${CREATE}`);
-		let {
-			username,
-			firstName,
-			lastName,
-			gender,
-			email,
-			password,
-			mobileNumber,
-			userImageSrc,
-			licenseImageSrc,
-			roleId
-		} = body;
-		let role = await db.Role.findByPk(roleId);
-		// Approve if userCreated is not guest
-		let approved = role.getDataValue("name") === ROLES.GUEST ? false : true;
+	let accessible = false;
+	let email = body.email;
+	if (req.body.inviteToken) {
+		// Consume invite token
+		let inviteToken = jwt.verify(req.body.inviteToken);
+		if (inviteToken) {
+			accessible = true;
+			email = inviteToken.email;
+		}
+	} else if (user && user.role && user.role.name) {
+		accessible = await accessControl.can(
+			user.role.name,
+			`${RESOURCES.USERS}:${CREATE}`
+		);
+	}
+	if (accessible) {
+		let role = await db.Role.findByPk(body.roleId);
+
+		// Immediately create the user otherwise.
+		let approved = role.getDataValue("name") === ROLES.GUEST ? false : true; // Approve if userCreated is not guest
 		let hashedPassword = await bcrypt.hash(password, 10);
-		let createdUser = await db.User.build({
-			username,
-			firstName,
-			lastName,
-			gender,
-			email,
-			password: hashedPassword,
-			mobileNumber,
-			userImageSrc,
-			licenseImageSrc,
-			roleId,
-			approved
-		});
 		try {
-			createdUser.validate();
-			createdUser = await createdUser.save();
-			response.setData({
-				id: createdUser.getDataValue("id"),
-				username,
-				firstName,
-				lastName,
-				gender,
+			let createdUser = await db.User.create({
+				...pickFields(
+					[
+						"username",
+						"firstName",
+						"lastName",
+						"gender",
+						"mobileNumber",
+						"userImageSrc",
+						"licenseImageSrc",
+						"roleId"
+					],
+					body
+				),
 				email,
-				mobileNumber,
-				userImageSrc,
-				licenseImageSrc,
-				roleId
+				approved,
+				password: hashedPassword
 			});
+			response.setData({
+				...pickFields(
+					[
+						"id",
+						"username",
+						"firstName",
+						"lastName",
+						"gender",
+						"email",
+						"mobileNumber",
+						"userImageSrc",
+						"licenseImageSrc"
+					],
+					createdUser
+				),
+				role: { id: role.id, name: role.name }
+			});
+
 			response.setMessage("User has been created.");
 			response.setCode(200);
 			response.setSuccess(true);
-			if (role.getDataValue("name") === ROLES.GUEST) {
-				// Send email invite
-				try {
-					await sendInviteToken(
-						createdUser.getDataValue("id"),
-						createdUser.getDataValue("email")
-					);
-				} catch (e) {
-					response.setMessage(e.message || "Cannot send invite.");
-					response.setCode(500);
-					response.setSuccess(false);
-				}
-			}
 		} catch (e) {
 			response.setMessage(e.message);
 			response.setCode(422);
@@ -147,142 +131,142 @@ router.post("/", async ({ user, body }, res) => {
 				e.errors.forEach(error => response.appendError(error.path));
 			}
 		}
-	} catch (err) {
+	} else {
 		response.setMessage(errorCodes.UNAUTHORIZED.message);
 		response.setCode(errorCodes.UNAUTHORIZED.statusCode);
 		res.status(errorCodes.UNAUTHORIZED.statusCode);
 	}
+
 	res.json(response);
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", requireLogin, disallowGuests, async (req, res) => {
 	let response = new ResponseBuilder();
-	try {
-		let foundUser = await db.User.findByPk(req.params.id, {
-			include: [{ model: db.Role, as: "role" }]
-		});
-		if (foundUser) {
-			let {
-				id,
-				username,
-				firstName,
-				lastName,
-				gender,
-				email,
-				mobileNumber,
-				lastLogin,
-				userImageSrc,
-				licenseImageSrc,
-				approved,
-				blocked,
-				createdAt,
-				updatedAt,
-				userCreatorId,
-				role
-			} = foundUser;
-			response.setData({
-				id,
-				username,
-				firstName,
-				lastName,
-				gender,
-				email,
-				mobileNumber,
-				lastLogin,
-				userImageSrc,
-				licenseImageSrc,
-				approved,
-				blocked,
-				createdAt,
-				updatedAt,
-				userCreatorId,
-				role: {
-					id: role.id,
-					name: role.name
-				}
+	let accessible = await accessControl.can(
+		user.role.name,
+		`${RESOURCES.USERS}:${READ}`
+	);
+	if (accessible) {
+		try {
+			let foundUser = await db.User.findByPk(req.params.id, {
+				include: [{ model: db.Role, as: "role" }]
 			});
-			response.setCode(200);
-			response.setMessage(`User with ID ${req.params.id} found.`);
-			response.setSuccess(true);
-		} else {
-			res.status(404);
-			response.setCode(404);
-			response.setMessage(`User with ID ${req.params.id} not found.`);
+			if (foundUser) {
+				let { role } = foundUser;
+				response.setData({
+					...pickFields(
+						[
+							"id",
+							"username",
+							"firstName",
+							"lastName",
+							"gender",
+							"email",
+							"mobileNumber",
+							"lastLogin",
+							"userImageSrc",
+							"licenseImageSrc",
+							"approved",
+							"blocked",
+							"createdAt",
+							"updatedAt",
+							"userCreatorId"
+						],
+						foundUser
+					),
+					role: {
+						id: role.id,
+						name: role.name
+					}
+				});
+				response.setCode(200);
+				response.setMessage(`User with ID ${req.params.id} found.`);
+				response.setSuccess(true);
+			} else {
+				res.status(404);
+				response.setCode(404);
+				response.setMessage(`User with ID ${req.params.id} not found.`);
+			}
+		} catch (e) {
+			res.status(errorCodes.UNAUTHORIZED.statusCode);
+			response.setCode(errorCodes.UNAUTHORIZED.statusCode);
+			response.setMessage(errorCodes.UNAUTHORIZED.message);
 		}
-	} catch (e) {
-		res.status(errorCodes.UNAUTHORIZED.statusCode);
-		response.setCode(errorCodes.UNAUTHORIZED.statusCode);
+	} else {
 		response.setMessage(errorCodes.UNAUTHORIZED.message);
+		response.setCode(errorCodes.UNAUTHORIZED.statusCode);
+		res.status(errorCodes.UNAUTHORIZED.statusCode);
 	}
+
 	res.json(response);
 });
 
-router.patch("/:id", async (req, res) => {
+router.patch("/:id", requireLogin, async (req, res) => {
 	let response = new ResponseBuilder();
-	try {
-		await accessControl.can(
-			req.user.role.name,
-			`${RESOURCES.USERS}:${UPDATE}`,
-			req.body
-		);
+
+	let accessible = await accessControl.can(
+		req.user.role.name,
+		`${RESOURCES.USERS}:${UPDATE}`,
+		{
+			updateUser: {
+				id: req.params.id
+			},
+			currentUser: {
+				id: req.user.id
+			}
+		}
+	);
+	if (accessible) {
 		let foundUser = await db.User.findByPk(req.params.id, {
 			include: [{ model: db.Role, as: "role" }]
 		});
 		if (foundUser) {
 			try {
-				let {
-					id,
-					username,
-					firstName,
-					lastName,
-					gender,
-					email,
-					mobileNumber,
-					lastLogin,
-					userImageSrc,
-					licenseImageSrc,
-					blocked,
-					createdAt,
-					updatedAt,
-					userCreatorId,
-					roleId
-				} = req.body;
 				let updatedUser = await foundUser.update(
-					{
-						id,
-						username,
-						firstName,
-						lastName,
-						gender,
-						email,
-						mobileNumber,
-						lastLogin,
-						userImageSrc,
-						licenseImageSrc,
-						blocked,
-						createdAt,
-						updatedAt,
-						userCreatorId,
-						roleId
-					},
+					pickFields(
+						[
+							"id",
+							"username",
+							"firstName",
+							"lastName",
+							"gender",
+							"email",
+							"mobileNumber",
+							"lastLogin",
+							"userImageSrc",
+							"licenseImageSrc",
+							"blocked",
+							"createdAt",
+							"updatedAt",
+							"userCreatorId",
+							"roleId"
+						],
+						req.body
+					),
 					{ include: [{ model: db.Role, as: "role" }] }
 				);
-				let { role } = updatedUser;
+				let role = updatedUser.getDataValue(role);
 				response.setData({
-					id,
-					username,
-					firstName,
-					lastName,
-					gender,
-					email,
-					mobileNumber,
-					lastLogin,
-					userImageSrc,
-					licenseImageSrc,
-					blocked,
-					createdAt,
-					updatedAt,
-					userCreatorId,
+					...pickFields(
+						[
+							"id",
+							"username",
+							"firstName",
+							"lastName",
+							"gender",
+							"email",
+							"mobileNumber",
+							"lastLogin",
+							"userImageSrc",
+							"licenseImageSrc",
+							"approved",
+							"blocked",
+							"createdAt",
+							"updatedAt",
+							"userCreatorId"
+						],
+						updatedUser.get({ plain: true })
+					),
 					role: {
 						id: role.id,
 						name: role.name
@@ -303,7 +287,7 @@ router.patch("/:id", async (req, res) => {
 			response.setCode(404);
 			response.setMessage(`User with ID ${req.params.id} not found.`);
 		}
-	} catch (e) {
+	} else {
 		response.setMessage(errorCodes.UNAUTHORIZED.message);
 		response.setCode(errorCodes.UNAUTHORIZED.statusCode);
 		res.status(errorCodes.UNAUTHORIZED.statusCode);
@@ -312,26 +296,36 @@ router.patch("/:id", async (req, res) => {
 	res.json(response);
 });
 
-router.delete("/:id", async ({ user, params }, res) => {
-	let response = new ResponseBuilder();
-	try {
-		await accessControl.can(user.role.name, `${RESOURCES.USERS}:${DELETE}`);
-		let foundUser = await db.User.findByPk(params.id);
-		if (foundUser) {
-			await foundUser.destroy();
-			response.setCode(200);
-			response.setSuccess(true);
-			response.setMessage(`User with ID ${params.id} has been deleted.`);
+router.delete(
+	"/:id",
+	requireLogin,
+	disallowGuests,
+	async ({ user, params }, res) => {
+		let response = new ResponseBuilder();
+
+		let accessible = await accessControl.can(
+			user.role.name,
+			`${RESOURCES.USERS}:${DELETE}`
+		);
+
+		if (accessible) {
+			let foundUser = await db.User.findByPk(params.id);
+			if (foundUser) {
+				await foundUser.destroy();
+				response.setCode(200);
+				response.setSuccess(true);
+				response.setMessage(`User with ID ${params.id} has been deleted.`);
+			} else {
+				response.setCode(404);
+				response.setMessage(`User with ID ${params.id} is not found.`);
+			}
 		} else {
-			response.setCode(404);
-			response.setMessage(`User with ID ${params.id} is not found.`);
+			response.setMessage(errorCodes.UNAUTHORIZED.message);
+			response.setCode(errorCodes.UNAUTHORIZED.statusCode);
+			res.status(errorCodes.UNAUTHORIZED.statusCode);
 		}
-	} catch (e) {
-		response.setMessage(errorCodes.UNAUTHORIZED.message);
-		response.setCode(errorCodes.UNAUTHORIZED.statusCode);
-		res.status(errorCodes.UNAUTHORIZED.statusCode);
+		res.json(response);
 	}
-	res.json(response);
-});
+);
 
 module.exports = router;
