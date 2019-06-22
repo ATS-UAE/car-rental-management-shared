39a4711269540523c919b836dd9f2d35feec7d66
ramilamparo <ramilamparo@gmail.com>
2019-06-22 16:27:08 +0400

    Refactor to use routes and add maintance action.

--- src/components/containers/display/VehicleCardList.js
+++ src/components/containers/display/VehicleCardList.js
@@ -1,63 +1,180 @@
 import React, { Fragment, useState } from "react";
-import { withRouter } from "react-router";
+import { withRouter, Route, Switch } from "react-router";
 import { compose } from "recompose";
 import { connect } from "react-redux";
-import { IconButton, withStyles } from "@material-ui/core";
-import { Edit, Visibility } from "@material-ui/icons";
+import { IconButton, Tooltip, withStyles } from "@material-ui/core";
+import { Edit, Visibility, Build } from "@material-ui/icons";
 
-import FormPage from "../../pages/FormPage";
+import BookingFormCreateMaintenance from "../forms/bookings/BookingFormCreateMaintenance";
 import VehicleFormUpdate from "../forms/vehicles/VehicleFormUpdate";
 import * as reduxActions from "../../../actions";
 import { actions, resources } from "../../../variables/enums";
 import { api } from "../../../utils";
+import { RBAC } from "../../../config/rbac";
 import CardList from "../../presentational/display/CardList";
+import Dialog from "../../presentational/display/Dialog";
 import Can from "../layout/Can";
 import VehicleBookingRange from "./VehicleBookingRange";
 
-function VehicleCardList({ vehicles, history, classes }) {
-	const [formData, setFormData] = useState({});
+function VehicleCardList({ vehicles, history, classes, auth, fetchVehicles }) {
+	const [formData, setFormData] = useState(null);
+	const renderDialog = ({ match, children }) => (
+		<Dialog
+			onMount={async () => {
+				try {
+					const vehicle = await api
+						.fetchVehicle(match.params.id)
+						.catch(() => history.replace("/vehicles"));
 
+					const read = {
+						access: await RBAC.can(
+							auth.data.role.name,
+							actions.READ,
+							resources.VEHICLES
+						),
+						exclude: RBAC.getExcludedFields(
+							auth.data.role.name,
+							actions.UPDATE,
+							resources.VEHICLES
+						)
+					};
+
+					const update = {
+						access: await RBAC.can(
+							auth.data.role.name,
+							actions.UPDATE,
+							resources.VEHICLES
+						),
+						exclude: RBAC.getExcludedFields(
+							auth.data.role.name,
+							actions.UPDATE,
+							resources.VEHICLES
+						)
+					};
+
+					const destroy = {
+						access: await RBAC.can(
+							auth.data.role.name,
+							actions.DELETE,
+							resources.VEHICLES
+						)
+					};
+
+					return {
+						vehicle,
+						read,
+						update,
+						destroy
+					};
+				} catch (e) {
+					history.replace("/vehicles");
+				}
+			}}
+			onClose={() => {
+				history.push("/vehicles");
+				setFormData(null);
+			}}
+			open={true}
+			children={children}
+		/>
+	);
 	return (
 		<Fragment>
-			<FormPage
-				check={({ location }) => /\/vehicles\/\d+/.test(location.pathname)}
-				path="/vehicles/:id"
-				exitPath="/vehicles"
-				onMount={({ location }) => setFormData(location.state.vehicle)}
-				render={({ location }) => {
-					let readOnly = [];
-					let exclude = [];
-					let showFooter = false;
-					if (location.state.updateAccess) {
-						exclude = location.state.readAccess.excludedFields;
-						readOnly = location.state.updateAccess.excludedFields;
-						showFooter = true;
-					} else {
-						exclude = location.state.readAccess.excludedFields;
-						readOnly = true;
-					}
-					let hints = readOnly === true ? "" : undefined;
-					return (
-						<VehicleFormUpdate
-							hints={hints}
-							values={formData}
-							onChangeEvent={(data, name, event) =>
-								event.target.files
-									? setFormData({
-											...data,
-											[name]: event.target.files[0] || ""
-									  })
-									: setFormData(data)
+			<Switch>
+				<Route
+					exact
+					path="/vehicles/:id(\d+)"
+					render={({ match }) =>
+						renderDialog({
+							match,
+							children: ({ vehicle, read }) => {
+								if (!formData && vehicle && vehicle.data) {
+									setFormData(vehicle.data);
+								} else if (formData && read && read.access) {
+									return (
+										<VehicleFormUpdate
+											hints={""}
+											values={formData}
+											onChangeEvent={(data, name, event) =>
+												event.target.files
+													? setFormData({
+															...data,
+															[name]: event.target.files[0] || ""
+													  })
+													: setFormData(data)
+											}
+											readOnly={true}
+											exclude={read.exclude}
+											onSubmit={() => history.push("/vehicles")}
+											showFooter={false}
+										/>
+									);
+								} else if (read && !read.access) {
+									history.replace("/vehicles");
+								}
+								return null;
 							}
-							readOnly={readOnly}
-							exclude={exclude}
-							onSubmit={() => history.push("/vehicles")}
-							showFooter={showFooter}
-						/>
-					);
-				}}
-			/>
+						})
+					}
+				/>
+				<Route
+					path="/vehicles/:id(\d+)/edit"
+					render={({ match }) =>
+						renderDialog({
+							match,
+							children: ({ vehicle, update, read }) => {
+								if (!formData && vehicle && vehicle.data) {
+									setFormData(vehicle.data);
+								} else if (formData && update && update.access) {
+									return (
+										<VehicleFormUpdate
+											hints={""}
+											values={formData}
+											onChangeEvent={(data, name, event) =>
+												event.target.files
+													? setFormData({
+															...data,
+															[name]: event.target.files[0] || ""
+													  })
+													: setFormData(data)
+											}
+											readOnly={update.exclude}
+											exclude={read.exclude}
+											onSubmit={() => history.push("/vehicles")}
+											showFooter={true}
+										/>
+									);
+								} else if (update && !update.access) {
+									history.replace("/vehicles");
+								}
 
+								return null;
+							}
+						})
+					}
+				/>
+				<Route
+					path="/vehicles/:id(\d+)/maintenance"
+					render={({ match }) =>
+						renderDialog({
+							match,
+							children: ({ vehicle, update }) => {
+								if (vehicle && vehicle.data && update && update.access) {
+									return (
+										<BookingFormCreateMaintenance
+											vehicle={vehicle.data}
+											onSubmit={() => history.push("/vehicles")}
+										/>
+									);
+								} else if (update && !update.access) {
+									history.replace("/vehicles");
+								}
+								return null;
+							}
+						})
+					}
+				/>
+			</Switch>
 			<CardList
 				details={cards => (
 					<VehicleBookingRange vehicleList={cards.map(card => card.vehicle)} />
@@ -91,35 +208,49 @@ function VehicleCardList({ vehicles, history, classes }) {
 									<Can
 										action={actions.UPDATE}
 										resource={resources.VEHICLES}
-										yes={updateAccess => (
-											<IconButton
-												onClick={() =>
-													api.fetchVehicle(vehicle.id).then(() =>
-														history.push(`/vehicles/${vehicle.id}/edit`, {
-															vehicle,
-															updateAccess,
-															readAccess
-														})
-													)
-												}
-											>
-												<Edit />
-											</IconButton>
-										)}
-										no={() => (
-											<IconButton
-												onClick={() => {
-													api.fetchVehicle(vehicle.id).then(() =>
-														history.push(`/vehicles/${vehicle.id}`, {
-															vehicle,
-															readAccess
-														})
-													);
-												}}
-											>
-												<Visibility />
-											</IconButton>
-										)}
+										yes={updateAccess => {
+											return (
+												<Fragment>
+													<Tooltip title="Edit">
+														<IconButton
+															onClick={() =>
+																history.push(`/vehicles/${vehicle.id}/edit`, {
+																	vehicle,
+																	updateAccess,
+																	readAccess
+																})
+															}
+														>
+															<Edit />
+														</IconButton>
+													</Tooltip>
+													<Tooltip title="Maintenance">
+														<IconButton
+															onClick={() =>
+																history.push(
+																	`/vehicles/${vehicle.id}/maintenance`
+																)
+															}
+														>
+															<Build />
+														</IconButton>
+													</Tooltip>
+												</Fragment>
+											);
+										}}
+										no={() => {
+											return (
+												<Tooltip title="View">
+													<IconButton
+														onClick={() =>
+															history.push(`/vehicles/${vehicle.id}`)
+														}
+													>
+														<Visibility />
+													</IconButton>
+												</Tooltip>
+											);
+										}}
 									/>
 								)}
 							/>
@@ -131,17 +262,17 @@ function VehicleCardList({ vehicles, history, classes }) {
 	);
 }
 
-const mapStateToProps = ({ vehicles }) => {
+const mapStateToProps = ({ vehicles, auth }) => {
 	let vehicleData = [];
 	if (vehicles && vehicles.success === true && vehicles.data) {
 		vehicleData = vehicles.data;
 	}
-	return { vehicles: vehicleData };
+	return { vehicles: vehicleData, auth };
 };
 
 const styles = {
 	card: {
-		height: "200px"
+		height: "300px"
 	}
 };
 
