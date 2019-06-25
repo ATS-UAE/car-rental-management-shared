dbccfca60e29487da88302ba966a3ad29c5d6139
ramilamparo <ramilamparo@gmail.com>
2019-06-25 11:07:47 +0400

    Add tabs and category editing

--- src/components/pages/Settings.js
+++ src/components/pages/Settings.js
@@ -1,13 +1,63 @@
-import React from "react";
-import { Paper } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { compose } from "recompose";
+import { connect } from "react-redux";
+import { Paper, Tabs, Tab } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 
+import * as actions from "../../actions";
 import PasswordChangeButtonDialog from "../containers/forms/PasswordChangeButtonDialog";
+import CategoryTableView from "../containers/display/CategoryTableView";
 
-function Settings({ classes }) {
+function Settings({
+	classes,
+	fetchCategories,
+	fetchCurrentUserDetails,
+	location,
+	match,
+	history,
+	auth
+}) {
+	useEffect(() => {
+		fetchCategories();
+		fetchCurrentUserDetails();
+	}, []);
+
+	const [value, setValue] = useState(0);
+	const handleChange = (event, newValue) => setValue(newValue);
+
+	const getTabContent = value => {
+		switch (value) {
+			case 0:
+				return <PasswordChangeButtonDialog />;
+			case 1:
+				if (auth && auth.data) {
+					return (
+						<CategoryTableView
+							location={location}
+							match={match}
+							history={history}
+						/>
+					);
+				} else return null;
+			default:
+				return null;
+		}
+	};
+
 	return (
 		<Paper className={classes.root}>
-			<PasswordChangeButtonDialog />
+			<Tabs
+				className={classes.tabs}
+				value={value}
+				onChange={handleChange}
+				indicatorColor="primary"
+				textColor="primary"
+				centered
+			>
+				<Tab label="Account" />
+				<Tab label="Misc." />
+			</Tabs>
+			<div>{getTabContent(value)}</div>
 		</Paper>
 	);
 }
@@ -20,7 +70,18 @@ const styles = theme => ({
 		},
 		height: "100%",
 		overflow: "auto"
+	},
+	tabs: {
+		marginBottom: theme.spacing(1)
 	}
 });
 
-export default withStyles(styles)(Settings);
+const mapStateToProps = ({ auth, categories }) => ({ auth, categories });
+
+export default compose(
+	connect(
+		mapStateToProps,
+		actions
+	),
+	withStyles(styles)
+)(Settings);
