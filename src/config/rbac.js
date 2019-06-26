08162aaa8dd668748e855eaf8f487d751655a7e5
ramilamparo <ramilamparo@gmail.com>
2019-06-26 15:57:39 +0400

    Update user config.

--- src/config/rbac.js
+++ src/config/rbac.js
@@ -62,7 +62,8 @@ generalRole.addPermission(
 	new Action(
 		UPDATE,
 		usersResource,
-		({ user, targetUser }) => user.id === targetUser.id
+		({ user, targetUser }) => user.id === targetUser.id,
+		["roleId"]
 	)
 );
 
@@ -140,14 +141,7 @@ adminRole.addPermission(new Action(CREATE, bookingsResource));
 
 // User permissions.
 adminRole.addPermission(new Action(CREATE, usersResource));
-adminRole.addPermission(
-	new Action(
-		UPDATE,
-		usersResource,
-		({ targetUser }) => targetUser && targetUser.role.name !== roleEnums.GUEST,
-		["password", "passwordConfirm"]
-	)
-);
+adminRole.addPermission(new Action(UPDATE, usersResource, null, ["roleId"]));
 adminRole.addPermission(new Action(DELETE, usersResource));
 
 // Accidents Permissions
