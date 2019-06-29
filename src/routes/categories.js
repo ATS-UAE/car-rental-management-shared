1055cb40b6ba73edb88fe3b991f103467a7b6a2b
ramilamparo <ramilamparo@gmail.com>
2019-06-29 12:37:12 +0400

    Add categories route.

---
+++ src/routes/categories.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router();
+const { RBAC } = require("../rbac/init");
+const db = require("../models");
+const { ResponseBuilder } = require("../utils");
+const requireLogin = require("../middlewares/requireLogin");
+
+router.get("/", requireLogin, async (req, res) => {
+	let response = new ResponseBuilder();
+	const categories = await db.Category.findAll();
+	response.setData(categories);
+	response.setSuccess(true);
+	response.setCode(200);
+	response.setMessage(null);
+	res.json(response);
+});
+
+router.post("/", requireLogin, async ({ body }, res) => {
+	let response = new ResponseBuilder();
+	const created = await db.Category.create({ name: body.name });
+	response.setData(created);
+	response.setSuccess(true);
+	response.setCode(200);
+	response.setMessage(null);
+	res.json(response);
+});
+
+router.patch("/:id", requireLogin, async ({ params, body }, res) => {
+	let response = new ResponseBuilder();
+	const found = await db.Category.findByPk(params.id);
+	found && found.update({ name: body.name });
+	response.setData(found);
+	response.setSuccess(true);
+	response.setCode(200);
+	response.setMessage(null);
+	res.json(response);
+});
+
+router.delete("/:id", requireLogin, async ({ params }, res) => {
+	let response = new ResponseBuilder();
+	const found = await db.Category.findByPk(params.id);
+	found && (await found.destroy());
+	response.setData(found);
+	response.setSuccess(true);
+	response.setCode(200);
+	response.setMessage(null);
+	res.json(response);
+});
+
+module.exports = router;
