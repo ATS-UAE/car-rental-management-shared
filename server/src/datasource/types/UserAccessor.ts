56e0994d7c59538e4c5edf037088d236a26933ea
Ramil Amparo <ramilamparo@gmail.com>
2019-09-29 06:21:32 +0400

    Hack tsc compilation

--- server/src/datasource/types/UserAccessor.ts
+++ server/src/datasource/types/UserAccessor.ts
@@ -1,11 +1,9 @@
 import { Role } from "../../variables/enums";
 
-// interface UserAccessor {
-// 	id: number;
-// 	role: { name: Role; id?: number };
-// 	[key: string]: any;
-// }
-
-type UserAccessor = any;
+interface UserAccessor {
+	id: number;
+	role: { name: Role; id?: number };
+	[key: string]: any;
+}
 
 export default UserAccessor;
