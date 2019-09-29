7063b6151c2cab11de0dac05c00780262cf804ee
Ramil Amparo <ramilamparo@gmail.com>
2019-09-29 06:10:31 +0400

    Change type

--- server/src/datasource/types/UserAccessor.ts
+++ server/src/datasource/types/UserAccessor.ts
@@ -1,9 +1,11 @@
 import { Role } from "../../variables/enums";
 
-interface UserAccessor {
-	id: number;
-	role: { name: Role; id?: number };
-	[key: string]: any;
-}
+// interface UserAccessor {
+// 	id: number;
+// 	role: { name: Role; id?: number };
+// 	[key: string]: any;
+// }
+
+type UserAccessor = any;
 
 export default UserAccessor;
