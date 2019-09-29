eff97c2b88aae48746c9b02e9e5f2dbc8439d4f9
Ramil Amparo <ramilamparo@gmail.com>
2019-09-29 06:05:06 +0400

    Rename

--- server/src/datasource/types/UserAccessor.ts
+++ server/src/datasource/types/UserAccessor.ts
@@ -1,9 +1,9 @@
 import { Role } from "../../variables/enums";
 
-type UserAccessor = {
+interface UserAccessor {
 	id: number;
 	role: { name: Role; id?: number };
 	[key: string]: any;
-};
+}
 
 export default UserAccessor;
