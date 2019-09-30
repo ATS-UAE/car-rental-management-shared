6c1c95967738d73f910136ce5453bc3316e4f7a7
Ramil Amparo <ramilamparo@gmail.com>
2019-09-30 17:36:19 +0400

    Change

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 cp .env server/
 cp .env client/
 
-PORT=48620 docker-compose -f docker-compose.yml build --no-cache
-PORT=48620 docker-compose -f docker-compose.yml up -d --force-recreate
+PORT=48620 docker-compose -f ./docker-compose.yml build --no-cache
+PORT=48620 docker-compose -f ./docker-compose.yml up -d --force-recreate
