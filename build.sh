ded59cf737e63ddcae747d25bfc93117f9c82b22
Ramil Amparo <ramilamparo@gmail.com>
2019-10-01 08:26:07 +0400

    Fix build.

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 cp .env server/
 cp .env client/
 
-PORT=48620 docker-compose -f car-rental-management/docker-compose.yml down
+PORT=48620 docker-compose -f docker-compose.yml down
 PORT=48620 docker-compose -f ./docker-compose.yml up -d -V --force-recreate
