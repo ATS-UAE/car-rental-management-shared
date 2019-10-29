a9f0962bc3ac9ff8ed1e2134da5db22cfd403e8f
Ramil Amparo <ramilamparo@gmail.com>
2019-10-29 07:14:36 +0400

    Modify migrations.

--- server/sequelize/migrations/20191028050331-booking-charge-estimate.ts
+++ server/sequelize/migrations/20191028050331-booking-charge-estimate.ts
@@ -6,25 +6,33 @@ module.exports = {
 		return Promise.all([
 			queryInterface.addColumn("Vehicles", "bookingChargeTime", {
 				type: Sequelize.INTEGER,
-				defaultValue: 0
+				defaultValue: 0,
+				allowNull: false
 			}),
 			queryInterface.addColumn("Vehicles", "bookingChargeDistance", {
-				type: Sequelize.INTEGER,
-				defaultValue: 0
+				type: Sequelize.FLOAT,
+				defaultValue: 0,
+				allowNull: false
 			}),
 			queryInterface.addColumn("Vehicles", "bookingCharge", {
-				type: Sequelize.INTEGER,
-				defaultValue: 0
+				type: Sequelize.FLOAT,
+				defaultValue: 0,
+				allowNull: false
+			}),
+			queryInterface.addColumn("Vehicles", "bookingChargeByTime", {
+				type: Sequelize.BOOLEAN,
+				defaultValue: true,
+				allowNull: false
 			})
 		]);
 	},
-
 	down: function(queryInterface: QueryInterface, Sequelize) {
 		// logic for transforming into the new state
 		return Promise.all([
 			queryInterface.removeColumn("Vehicles", "bookingChargeTime"),
 			queryInterface.removeColumn("Vehicles", "bookingChargeDistance"),
-			queryInterface.removeColumn("Vehicles", "bookingCharge")
+			queryInterface.removeColumn("Vehicles", "bookingCharge"),
+			queryInterface.removeColumn("Vehicles", "bookingChargeByTime")
 		]);
 	}
 };
