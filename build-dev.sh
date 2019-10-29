75306131c1769a772b92db1d9bc9c38cbfa24540
Ramil Amparo <ramilamparo@gmail.com>
2019-10-29 07:15:27 +0400

    Modify migrations.

--- build-dev.sh
+++ build-dev.sh
@@ -2,6 +2,4 @@
 cp .env server/
 cp .env client/
 
-PORT=48620 docker-compose -f docker-compose.dev.yml build --no-cache
-PORT=48620 docker-compose -f docker-compose.dev.yml up -d --force-recreate
-
+PORT=48620 docker-compose -f docker-compose.dev.yml up
