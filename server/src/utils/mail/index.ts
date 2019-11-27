ab997c5689210f1c27f3a9dd114d6eb6443bb0fc
Ramil Amparo <ramilamparo@gmail.com>
2019-11-28 02:59:14 +0400

    Move utils.

--- server/src/utils/mail/index.ts
+++ server/src/utils/mail/index.ts
@@ -10,7 +10,7 @@ import config from "../../config";
 import {
 	getStaticFilesPath,
 	makeDirectoryIfNotExist
-} from "../../utils/helpers";
+} from "../utils";
 
 const { mail, secretKey } = config;
 
