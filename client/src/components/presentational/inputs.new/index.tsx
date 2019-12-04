24e94eac16029a6273c074157c11daa669507684
Ramil Amparo <ramilamparo@gmail.com>
2019-12-04 14:54:43 +0400

    Remove unneeded code.

--- client/src/components/presentational/inputs.new/index.tsx
+++
@@ -1,6 +0,0 @@
-// export { default as MultiSelect } from "./MultiSelect";
-// export { default as ImageInput } from "./ImageInput";
-// export { default as DateTimePicker } from "./DateTimePicker";
-// export { default as Select } from "./Select";
-// export { default as Slider } from "./Slider";
-// export { default as TextField } from "./TextField";
