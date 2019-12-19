b658a363cd98708931794579b4260b4ac21b26d6
Ramil Amparo <ramilamparo@gmail.com>
2019-12-19 16:28:34 +0400

    Add Accident validators.

---
+++ server/src/api/validators/Accident.ts
@@ -0,0 +1,50 @@
+import * as yup from "yup";
+import { Op } from "sequelize";
+import { Booking, User, Vehicle } from "../../models";
+import { Role } from "../../variables/enums";
+import moment from "moment";
+
+export abstract class Accident {
+	static create = yup
+		.object()
+		.shape({
+			message: yup.string().required(),
+			accidentImageSrc: yup.string(),
+			lat: yup.string().required(),
+			lng: yup.string().required(),
+			userId: yup.string().required(),
+			vehicleId: yup.string().required(),
+			bookingId: yup.string().required()
+		})
+		.test(
+			"permission",
+			"You do not have the permission to do this.",
+			async function(v) {
+				const user = this.options.context["user"] as User;
+
+				if (user.role === Role.GUEST) {
+					const userBookings = await user.$get("bookings", {
+						where: {
+							from: {
+								[Op.lte]: moment().toDate()
+							},
+							to: {
+								[Op.gte]: moment().toDate()
+							},
+							approved: true,
+							finished: false
+						}
+					});
+					for (const booking of userBookings) {
+						if (
+							booking.vehicleId === v.vehicleId &&
+							booking.id === v.bookingId
+						) {
+							return true;
+						}
+					}
+				}
+				return false;
+			}
+		);
+}
