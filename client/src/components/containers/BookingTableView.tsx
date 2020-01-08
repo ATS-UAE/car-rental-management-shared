a2c7b4d09a9a7bb07824aaed82a741b53d96c35f
Ramil Amparo <ramilamparo@gmail.com>
2020-01-08 21:32:32 +0400

    WIP

---
+++ client/src/components/containers/BookingTableView.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from "react";
+import { MaterialTable } from "../presentational";
+
+export const BookingTableView: FC<{}> = () => {
+	return null;
+};
---
+++ client/src/components/presentational/Modal.tsx
@@ -0,0 +1,58 @@
+import React, { FC } from "react";
+import classNames from "classnames";
+import { useLocation, Route } from "react-router-dom";
+import { Dialog, Theme, WithStyles, createStyles } from "@material-ui/core";
+import { Loading } from ".";
+
+export interface ModalProps extends WithStyles<typeof styles> {
+	path?: string;
+	onClose: () => void;
+	open: boolean;
+	loading?: boolean;
+}
+
+export const Modal: FC<ModalProps> = ({
+	children,
+	path,
+	onClose,
+	open,
+	loading,
+	classes
+}) => {
+	const location = useLocation<{ background?: boolean }>();
+
+	const background = location && location.state && location.state.background;
+
+	const modalComponent = (
+		<Dialog
+			onClose={onClose}
+			open={open}
+			classes={{
+				paper: classNames(classes.paper, {
+					[classes.loading]: Boolean(loading)
+				})
+			}}
+		>
+			{loading ? <Loading /> : children}
+		</Dialog>
+	);
+
+	return path && background ? (
+		<Route path={path} children={modalComponent}></Route>
+	) : (
+		modalComponent
+	);
+};
+
+const styles = (theme: Theme) =>
+	createStyles({
+		paper: {
+			padding: theme.spacing(2)
+		},
+		loading: {
+			padding: theme.spacing(4),
+			overflow: "visible",
+			boxShadow: "none",
+			backgroundColor: "rgba(0, 0, 0, 0)"
+		}
+	});
