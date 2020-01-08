2359630922fbbba7b050e4f0758641c9a11130b5
Ramil Amparo <ramilamparo@gmail.com>
2020-01-08 12:02:52 +0400

    Move email helpers to api. Stronger typing.

--- server/src/api/Booking.ts
+++ server/src/api/Booking.ts
@@ -1,17 +1,27 @@
-import { ValidationError } from "yup";
+import { Wialon } from "node-wialon";
+import { Op } from "sequelize";
+import moment = require("moment");
+
 import { Booking as BookingValidators } from "./validators";
 import {
 	User,
 	Booking as BookingModel,
 	ReplaceVehicle,
 	BookingAttributes,
-	ReplaceVehicleAttributes
+	ReplaceVehicleAttributes,
+	Location,
+	Vehicle
 } from "../models";
 import { ItemNotFoundException } from "./exceptions";
 import { UseParameters } from ".";
 import { Role } from "../variables/enums";
 import { ApiErrorHandler } from "./utils";
 import { Castable, Collection } from "./Collection";
+import {
+	sendBookingNotification,
+	sendInvoice as sendInvoiceEmail,
+	sendBookingConfirmation as sendBookingConfirmationEmail
+} from "../utils/mail";
 
 export type BookingCreateOptions = UseParameters<
 	BookingAttributes,
@@ -163,5 +173,114 @@ export class Booking implements Castable<Partial<BookingAttributes>> {
 		} catch (e) {
 			new ApiErrorHandler(e);
 		}
+	};
+
+	public setEmailNotificationsToBookingManagers = async () => {
+		const bookingData = await this.data.reload({
+			include: [{ model: User }]
+		});
+		await User.findAll({
+			where: {
+				clientId: bookingData.user.clientId,
+				role: {
+					[Op.in]: [Role.ADMIN, Role.KEY_MANAGER]
+				}
+			}
+		}).then(async users => {
+			const vehicle = await Vehicle.findByPk(bookingData.vehicleId);
+			const location = await Location.findByPk(vehicle.locationId);
+
+			let lng = location.lng;
+			let lat = location.lat;
+
+			if (vehicle.wialonUnitId) {
+				const w = await Wialon.login({
+					token: process.env.WIALON_TOKEN
+				});
+				const unit = await w.Core.searchItem({
+					id: vehicle.wialonUnitId,
+					flags: 1024 + 8192
+				});
+				if (unit) {
+					lat = unit.item && unit.item.pos && unit.item.pos.y;
+					lng = unit.item && unit.item.pos && unit.item.pos.x;
+				}
+			}
+			for (const user of users) {
+				try {
+					sendBookingNotification({
+						email: user.email,
+						company: "LeasePlan",
+						bookingId: bookingData.id,
+						customerEmail: bookingData.user.email,
+						customerName: `${bookingData.user.firstName} ${bookingData.user.lastName}`,
+						from: moment(bookingData.from).unix(),
+						to: moment(bookingData.to).unix(),
+						lat,
+						lng,
+						location: location.name,
+						mobile: bookingData.user.mobileNumber,
+						plateNumber: vehicle.plateNumber || "N/A",
+						vehicle: `${vehicle.brand} ${vehicle.model}`,
+						vehicleId: vehicle.id,
+						timeZone: user.timeZone
+					});
+				} catch (e) {}
+			}
+		});
+	};
+
+	public sendInvoice = async (amount: number) => {
+		const bookingData = await this.data.reload({
+			include: [{ model: User }, { model: Vehicle }]
+		});
+		await sendInvoiceEmail({
+			email: bookingData.user.email,
+			amount: amount,
+			customerName: bookingData.user.firstName,
+			vehicleName: `${bookingData.vehicle.brand} ${bookingData.vehicle.model}`,
+			from: moment(bookingData.from, "X").unix(),
+			to: moment(bookingData.to, "X").unix(),
+			bookingId: bookingData.id,
+			timeZone: bookingData.user.timeZone
+		});
+	};
+
+	public sendBookingConfirmation = async () => {
+		const bookingData = await this.data.reload({
+			include: [{ model: User }, { model: Vehicle }]
+		});
+		const vehicleLocation = await Location.findByPk(
+			bookingData.vehicle.locationId
+		);
+		let lng = vehicleLocation.lng;
+		let lat = vehicleLocation.lat;
+
+		if (bookingData.vehicle.wialonUnitId) {
+			const w = await Wialon.login({
+				token: process.env.WIALON_TOKEN
+			});
+			const unit = await w.Core.searchItem({
+				id: bookingData.vehicle.wialonUnitId,
+				flags: 1024 + 8192
+			});
+			if (unit) {
+				lat = unit.item && unit.item.pos && unit.item.pos.y;
+				lng = unit.item && unit.item.pos && unit.item.pos.x;
+			}
+		}
+		await sendBookingConfirmationEmail({
+			email: bookingData.user.email,
+			customerName: bookingData.user.firstName,
+			vehicleName: `${bookingData.vehicle.brand} ${bookingData.vehicle.model} ${bookingData.vehicle.plateNumber}`,
+			from: moment(bookingData.from, "X").unix(),
+			to: moment(bookingData.to, "X").unix(),
+			bookingId: bookingData.id,
+			address: vehicleLocation && vehicleLocation.address,
+			parkingLocation: bookingData.vehicle.parkingLocation,
+			lat,
+			lng,
+			timeZone: bookingData.user.timeZone
+		});
 	};
 }
--- server/src/routes/bookings.ts
+++ server/src/routes/bookings.ts
@@ -1,31 +1,43 @@
 import express from "express";
 import requireLogin from "../middlewares/requireLogin";
-import { Booking } from "../api";
-import { Location } from "../models";
+import { Booking, BookingCreateOptions, BookingUpdateOptions } from "../api";
+import { BookingAttributes } from "../models";
 import { ResponseBuilder } from "../utils";
-import { sendInvoice, sendBookingConfirmation } from "../utils/mail";
 
-import moment from "moment";
 const router = express.Router();
+
 router.use(requireLogin);
 
-router.get("/", async ({ user }: any, res) => {
-	const response = new ResponseBuilder();
-	try {
-		const foundBookings = await Booking.getAll(user);
-		response.handleSuccess(`Found ${foundBookings.data.length} bookings.`, res);
-		response.setData(foundBookings.cast(user));
-	} catch (e) {
-		response.handleError(e, res);
+router.get<undefined, ResponseBuilder<Partial<BookingAttributes>[]>, undefined>(
+	"/",
+	async ({ user }, res) => {
+		const response = new ResponseBuilder<Partial<BookingAttributes>[]>();
+		try {
+			const foundBookings = await Booking.getAll(user);
+			response.handleSuccess(
+				`Found ${foundBookings.data.length} bookings.`,
+				res
+			);
+			response.setData(foundBookings.cast(user));
+		} catch (e) {
+			response.handleError(e, res);
+		}
+
+		res.json(response);
 	}
+);
 
-	res.json(response);
-});
-
-router.post("/", async ({ user, body }: any, res) => {
-	const response = new ResponseBuilder();
+router.post<
+	undefined,
+	ResponseBuilder<Partial<BookingAttributes>>,
+	BookingCreateOptions
+>("/", async ({ user, body }, res) => {
+	const response = new ResponseBuilder<Partial<BookingAttributes>>();
 	try {
 		const createdBooking = await Booking.create(user, body);
+
+		createdBooking.setEmailNotificationsToBookingManagers();
+
 		response.setData(createdBooking.cast(user));
 		response.handleSuccess("Booking has been created.", res);
 	} catch (e) {
@@ -34,8 +46,12 @@ router.post("/", async ({ user, body }: any, res) => {
 	res.json(response);
 });
 
-router.get("/:id", async ({ user, params }: any, res) => {
-	let response = new ResponseBuilder();
+router.get<
+	{ id: string },
+	ResponseBuilder<Partial<BookingAttributes>>,
+	undefined
+>("/:id", async ({ user, params }: any, res) => {
+	const response = new ResponseBuilder<Partial<BookingAttributes>>();
 	try {
 		let foundBooking = await Booking.get(user, params.id);
 		response.setData(foundBooking.cast(user));
@@ -46,46 +62,25 @@ router.get("/:id", async ({ user, params }: any, res) => {
 	res.json(response);
 });
 
-router.patch("/:id", async ({ user, params, body }: any, res) => {
-	const response = new ResponseBuilder();
+router.patch<
+	{ id: string },
+	ResponseBuilder<Partial<BookingAttributes>>,
+	BookingUpdateOptions
+>("/:id", async ({ user, params, body }: any, res) => {
+	const response = new ResponseBuilder<Partial<BookingAttributes>>();
 	try {
 		const bookingPreviousValue = await Booking.get(user, params.id);
 		const updatedBooking = await bookingPreviousValue.update(user, body);
-		const bookingData = await updatedBooking.data.reload({
-			include: [{ all: true }]
-		});
 
 		if (
 			body.amount !== undefined &&
 			body.amount !== null &&
 			bookingPreviousValue.data.amount === null
 		) {
-			sendInvoice({
-				email: bookingData.user.email,
-				amount: body.amount,
-				customerName: bookingData.user.firstName,
-				vehicleName: `${bookingData.vehicle.brand} ${bookingData.vehicle.model}`,
-				from: moment(bookingData.from, "X").unix(),
-				to: moment(bookingData.to, "X").unix(),
-				bookingId: bookingData.id,
-				timeZone: bookingData.user.timeZone
-			});
+			updatedBooking.sendInvoice(body.amount);
 		}
 		if (body.approved === true && bookingPreviousValue.data.approved === null) {
-			const location = await Location.findByPk(bookingData.vehicle.locationId);
-			sendBookingConfirmation({
-				email: bookingData.user.email,
-				customerName: bookingData.user.firstName,
-				vehicleName: `${bookingData.vehicle.brand} ${bookingData.vehicle.model} ${bookingData.vehicle.plateNumber}`,
-				from: moment(bookingData.from, "X").unix(),
-				to: moment(bookingData.to, "X").unix(),
-				bookingId: bookingData.id,
-				address: location && location.address,
-				parkingLocation: bookingData.vehicle.parkingLocation,
-				lat: location && location.lat,
-				lng: location && location.lng,
-				timeZone: bookingData.user.timeZone
-			});
+			updatedBooking.sendBookingConfirmation();
 		}
 
 		response.setData(updatedBooking.cast(user));
@@ -96,8 +91,12 @@ router.patch("/:id", async ({ user, params, body }: any, res) => {
 	res.json(response);
 });
 
-router.delete("/:id", async ({ user, params }: any, res) => {
-	const response = new ResponseBuilder();
+router.delete<
+	{ id: string },
+	ResponseBuilder<Partial<BookingAttributes>>,
+	undefined
+>("/:id", async ({ user, params }: any, res) => {
+	const response = new ResponseBuilder<Partial<BookingAttributes>>();
 	try {
 		const foundBooking = await Booking.get(user, params.id);
 		await foundBooking.destroy(user);
