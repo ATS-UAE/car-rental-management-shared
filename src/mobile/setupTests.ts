210a242adeeb8eea66b1bb788a7bfbbc1ce71578
ram <ramilamparo@gmail.com>
2020-04-18 08:57:54 +0400

    First commit.

---
+++ src/mobile/setupTests.ts
@@ -0,0 +1,4 @@
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+console.log(Enzyme);
+Enzyme.configure({ adapter: new Adapter() });
---
+++ src/mobile/src/components/presentational/tests/VehicleList.test.tsx
@@ -0,0 +1,5 @@
+describe("This test", () => {
+	it("Does something", () => {
+		expect(1 + 1).toEqual(2);
+	});
+});
