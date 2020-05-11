8597350fa56ec8a433bfffd64210dc92c3baa654
Ramil Amparo <ramilamparo@gmail.com>
2020-05-11 21:55:17 +0400

    Do not allow null response.

--- typings/api/WialonUnit.ts
+++ typings/api/WialonUnit.ts
@@ -1,6 +1,7 @@
 import { ServerResponse } from "../";
 
 export interface WialonUnitAttributes {
+	id: number;
 	name: string;
 	imei: string;
 	lat: number | null;
@@ -8,7 +9,7 @@ export interface WialonUnitAttributes {
 	mileage: number | null;
 }
 
-export type WialonUnitServerResponseGet = ServerResponse<WialonUnitAttributes | null>;
+export type WialonUnitServerResponseGet = ServerResponse<WialonUnitAttributes>;
 export type WialonUnitServerResponseGetAll = ServerResponse<
 	WialonUnitAttributes[]
 >;
