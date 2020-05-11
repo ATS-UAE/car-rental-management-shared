af97e9a81cb1084971e3d37fb69b5419a7786b48
Ramil Amparo <ramilamparo@gmail.com>
2020-05-11 07:10:43 +0400

    Merge latest.

--- typings/api/User.ts
+++ typings/api/User.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
 	UserAttributes,
 	DatePropsToUnix,
@@ -48,54 +47,3 @@ export type UserServerParamsPost = DatePropsToUnix<
 export type UserServerResponsePost = UserServerResponseGet;
 export type UserServerResponsePatch = UserServerResponseGet;
 export type UserServerResponseDelete = UserServerResponseGet;
-=======
-import {
-	UserAttributes,
-	DatePropsToUnix,
-	ServerResponse,
-	RemoveImmutableSequelizeProperties,
-	UseParameters
-} from "../";
-
-export type UserServerResponseGet = ServerResponse<
-	DatePropsToUnix<UserAttributes>
->;
-export type UserServerResponseGetAll = ServerResponse<
-	DatePropsToUnix<UserAttributes>[]
->;
-export type UserServerParamsPatch = DatePropsToUnix<
-	UseParameters<
-		RemoveImmutableSequelizeProperties<UserAttributes>,
-		never,
-		| "userImageSrc"
-		| "timeZone"
-		| "clientId"
-		| "email"
-		| "firstName"
-		| "lastName"
-		| "blocked"
-		| "mobileNumber"
-		| "licenseImageSrc"
-		| "password"
-		| "role"
-		| "username"
-	> & { categories: number[] }
->;
-export type UserServerParamsPost = DatePropsToUnix<
-	UseParameters<
-		RemoveImmutableSequelizeProperties<UserAttributes>,
-		| "clientId"
-		| "email"
-		| "username"
-		| "firstName"
-		| "lastName"
-		| "password"
-		| "mobileNumber"
-		| "role",
-		"userImageSrc" | "timeZone"
-	>
-> & { categories: number[] };
-export type UserServerResponsePost = UserServerResponseGet;
-export type UserServerResponsePatch = UserServerResponseGet;
-export type UserServerResponseDelete = UserServerResponseGet;
->>>>>>> ef4cef8ba944e25a319d7e4a1503ced66687246a
