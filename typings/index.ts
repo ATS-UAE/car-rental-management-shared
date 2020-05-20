ef26420657c2039780d992c1df004f7a1774e28b
Ramil Amparo <ramilamparo@gmail.com>
2020-05-20 18:47:58 +0400

    Remove OPERATION enum types.

--- typings/index.ts
+++ typings/index.ts
@@ -3,14 +3,6 @@ export * from "./enums";
 export * from "./models";
 export * from "./api";
 
-// TODO remove RBAC
-export enum Operation {
-	READ = "READ",
-	UPDATE = "UPDATE",
-	DELETE = "DELETE",
-	CREATE = "CREATE"
-}
-
 export enum Resource {
 	BOOKINGS = "BOOKINGS",
 	LOCATIONS = "LOCATIONS",
