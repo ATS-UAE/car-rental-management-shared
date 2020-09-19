1fc5eb0a266bbb80bbac9f7cb060e36922638cb7
Ramil Amparo <ramilamparo@gmail.com>
2020-09-19 14:15:04 +0400

    Remove unused declarations.

--- src/typings/websockets/Booking.ts
+++
@@ -1,7 +0,0 @@
-import type { SocketEvent, SocketEventName } from ".";
-import { BookingServerResponsePatch } from "../api";
-
-export type SocketEventBookingUpdate = SocketEvent<
-	SocketEventName.BOOKING_UPDATE,
-	BookingServerResponsePatch["data"]
->;
--- src/typings/websockets/index.ts
+++ src/typings/websockets/index.ts
@@ -1,10 +1,3 @@
-export * from "./Booking";
-
 export enum SocketEventName {
 	BOOKING_UPDATE = "booking_update"
-}
-
-export interface SocketEvent<EventName extends SocketEventName, Data> {
-	name: EventName;
-	data: Data;
 }
