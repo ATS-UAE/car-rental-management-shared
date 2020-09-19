ace40c35fc8955d7921ebe2c351c7278a9971007
Ramil Amparo <ramilamparo@gmail.com>
2020-09-19 13:09:01 +0400

    Remove resource enum.

--- typings/index.ts
+++ typings/index.ts
@@ -3,15 +3,3 @@ export * from "./enums";
 export * from "./models";
 export * from "./api";
 export * from "./websockets";
-
-export enum Resource {
-	BOOKINGS = "BOOKINGS",
-	LOCATIONS = "LOCATIONS",
-	VEHICLES = "VEHICLES",
-	USERS = "USERS",
-	ENUMS = "ENUMS",
-	INVITES = "INVITES",
-	ACCIDENTS = "ACCIDENTS",
-	CLIENTS = "CLIENTS",
-	CATEGORIES = "CATEGORIES"
-}
