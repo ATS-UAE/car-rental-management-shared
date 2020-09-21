f6391cc06ac7dcf0368929a05d090db4fcf5c1ff
Ramil Amparo <ramilamparo@gmail.com>
2020-09-21 07:49:52 +0400

    Update booking event payload data.

--- src/typings/websockets/Booking.ts
+++ src/typings/websockets/Booking.ts
@@ -1,14 +1,18 @@
 import type { SocketEvent, SocketEventName } from ".";
+import { BookingAttributes } from "../models";
 
 /**
  * Sends the approval status of the booking.
  */
-export type BookingApprovalEventData = boolean | null;
+export type BookingApprovalEventData = Pick<
+	BookingAttributes,
+	"id" | "approved"
+>;
 
 /**
  * Sends the ID of the new booking.
  */
-export type BookingCreateEventData = number;
+export type BookingCreateEventData = Pick<BookingAttributes, "id">;
 
 export type BookingApprovalEvent = SocketEvent<
 	SocketEventName.BOOKING_APPROVAL,
